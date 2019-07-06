3e2d74c56647aaf2b62a421e04138e60b8261fee
kobayashi <kobayashi@Otocin.local>
2019-07-07 02:52:07 +0900

    feat: Play class

--- hole.py
+++ hole.py
@@ -10,11 +10,18 @@ class PlayerHole(Hole):
         self.num_stone = num_stone
         self.position = position
 
-    def sow(self, board, position):
+    def sow(self, board, position, num_hole):
         rotation = -(-self.num_stone // len(board)) + 1
+        print("rotation:", rotation)
         board = board * rotation
-        begin = position
+        if(position <= int(num_hole/2)):
+            begin = position
+        else:
+            begin = position + 1
         final = begin + self.num_stone
+        print("begin:", begin)
+        print("final:", final)
+        print("length:", len(board))
         self.num_stone = 0
         for hole in board[begin:final]:
             hole.sowed()
--- play.py
+++ play.py
@@ -1,24 +1,40 @@
 from hole import *
 
-def generate_board(num_hole, init_stone):
-    board = []
-    for position in range(1, num_hole+1):
-        board.append(PlayerHole(init_stone, position))
-    board.insert(int(num_hole/2), SideHole("right"))
-    board.append(SideHole("left"))
+class Play():    
+    def generate_board(self, num_hole, init_stone):
+        board = []
+        for position in range(1, num_hole+1):
+            board.append(PlayerHole(init_stone, position))
+        board.insert(int(num_hole/2), SideHole("right"))
+        board.append(SideHole("left"))
 
-    return board
+        return board
+    
+    def __init__(self, num_hole, init_stone):
+        self.board = self.generate_board(num_hole, init_stone)
+        self.num_hole = num_hole
+        self.init_stone = init_stone
 
+    def sowing(self, position):
+        print("---------------sowing(", position, ")---------------")
+        if(position <= int(self.num_hole/2)):
+            self.board[position-1].sow(self.board, position, self.num_hole)
+        else:
+            self.board[position].sow(self.board, position, self.num_hole)
 
-def sowing(board, position):
-    board[position-1].sow(board, position)
+    def view_board(self):
+        for hole in self.board:
+            print(hole.num_stone)
 
-def view_board(board):
-    for hole in board:
-        print(hole.num_stone)
-
-board = generate_board(12, 2)
-board[3].num_stone = 28
-
-sowing(board, 4)
-view_board(board)
+if __name__ == "__main__":
+    num_hole = 6
+    init_stone = 200
+    play = Play(num_hole, init_stone)
+    play.sowing(3)
+    play.view_board()
+    play.sowing(5)
+    play.view_board()
+    play.sowing(1)
+    play.view_board()
+    play.sowing(6)
+    play.view_board()
