a9ec6f7929c451873e6e05473b82d64a0ca07a0b
kobayashi <kobayashi@Otocin.local>
2019-07-13 18:05:06 +0900

    continue own turn when ending at side hole

--- play.py
+++ play.py
@@ -23,11 +23,20 @@ class Play():
         print("please input the position number where you want to move stones there")
         position = int(input("position number>> "))
         active_board = self.player_side if self.turn=="player" else self.opponent_side
-        active_board[position-1].sow(self.board, position, self.num_hole, self.turn)
-        self.change_turn()
+        selected_hole = active_board[position-1]
+        num_sowed_stone = selected_hole.num_stone
+        selected_hole.sow(self.board, position, self.num_hole, self.turn)
+        self.change_turn(position, num_sowed_stone)
     
-    def change_turn(self):
-        self.turn = "player" if self.turn=="opponent" else "opponent" 
+    def change_turn(self, position, num_sowed_stone):
+        distance_to_sidehole = self.num_hole - position + 1
+        print(distance_to_sidehole, num_sowed_stone)
+        if(distance_to_sidehole == num_sowed_stone):
+            pass
+        else:
+            self.turn = "player" if self.turn=="opponent" else "opponent" 
+        self.view_board()
+        self.sowing()
 
     def view_board(self):
         for hole in reversed(self.opponent_side):
